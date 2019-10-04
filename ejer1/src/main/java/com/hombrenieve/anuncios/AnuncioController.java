3afe2a022eed7bd88f1165c0af061aee4217387e
Pablo Rebaque <pablo.rebaque.diaz@gmail.com>
2019-10-04 16:27:22 +0200

    Exercise 4: Added logic
    We use the get method to create the ad since we need it to return a static web page

--- ejer1/src/main/java/com/hombrenieve/anuncios/AnuncioController.java
+++ ejer1/src/main/java/com/hombrenieve/anuncios/AnuncioController.java
@@ -2,16 +2,43 @@ package com.hombrenieve.anuncios;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class AnuncioController {
 
-    @PostMapping("/newAd")
+    private List<Advertisement> adverts = new ArrayList<Advertisement>();
+
+
+    @PostConstruct
+    public void init() {
+        adverts.add(new Advertisement("Pablo", "Vendo moto", "Pocos kms"));
+        adverts.add(new Advertisement("Paco", "Vendo mesa", "impecable"));
+        adverts.add(new Advertisement("Juan", "Vendo casa", "bueno estado"));
+    }
+
+
+    @GetMapping("/newAd")
     public String newAd(Model model, Advertisement ad) {
-        model.addAttribute("adv", ad);
+        adverts.add(ad);
+        return "/anuncioGuardado.html";
 
+    }
+
+    @GetMapping("/")
+    public String ads(Model model) {
+        model.addAttribute("adverts", adverts);
+        return "listaAnuncios";
+    }
+
+    @GetMapping("/advs/{id}")
+    public String ad(Model model, @PathVariable int id) {
+        Advertisement adv = adverts.get(id-1);
+        model.addAttribute("adv", adv);
         return "anuncio";
-
     }
 }
